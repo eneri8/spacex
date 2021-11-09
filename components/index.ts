284e7982dcc984e40352c5412137c6af9ebeccb4
Edson Neri <elmismisimoneri@gmail.com>
2021-11-09 06:25:24 -0600

    adds `Page` component

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,9 @@
 import { default as MissionCard } from './MissionCard';
 import { default as MissionList } from './MissionList';
+import { default as Page } from './Page';
 
 export {
     MissionCard,
     MissionList,
+    Page,
 };
