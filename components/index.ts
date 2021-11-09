f112e7f58c38a4c98c72ad31c3d32a343d146f25
Edson Neri <elmismisimoneri@gmail.com>
2021-11-09 06:26:21 -0600

    adds `Header` component

--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,10 @@
+import { default as Header } from './Header';
 import { default as MissionCard } from './MissionCard';
 import { default as MissionList } from './MissionList';
 import { default as Page } from './Page';
 
 export {
+    Header,
     MissionCard,
     MissionList,
     Page,
