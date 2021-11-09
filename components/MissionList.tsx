250a344cd2ccf5f08c36c861aeef25923e125c48
Edson Neri <elmismisimoneri@gmail.com>
2021-11-09 06:18:24 -0600

    adds `MissionList` components
    closes #5

---
+++ components/MissionList.tsx
@@ -0,0 +1,25 @@
+import { SimpleGrid, Container } from "@chakra-ui/react";
+import MissionCard, { Mission } from '../components/MissionCard';    
+
+interface Props {
+  missions: Mission[]
+} 
+
+const MissionList = ({ missions }: Props): JSX.Element => {
+  return (
+    <>
+      <SimpleGrid columns={[1, 2, 3, 4, 5]}>
+        {
+          missions.map((mission) => (
+              <MissionCard
+                  key={mission.id}
+                  mission={mission}
+              />
+          ))
+        }
+      </SimpleGrid>
+    </>
+  );
+}
+
+export default MissionList;
